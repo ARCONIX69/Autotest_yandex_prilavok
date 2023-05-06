35888d8a71cbc4d2196f7b7acf404e78a395f309
ARCONIX69 <yurchick351@gmail.com>
2023-05-06 11:48:23 +0300

    My first commit

---
+++ configuration.py
@@ -0,0 +1,8 @@
+URL_SERVICE = "https://d9915eb4-5ca5-41c4-b153-fa277faf58f3.serverhub.praktikum-services.ru"
+DOC_PATH = "/docs/"
+
+LOG_MAIN_PATH = "/api/logs/main/"
+USER_TABLE_PATH = "/api/db/resources/user_model.csv"
+
+CREATE_USERS_PATH = "/api/v1/users/"
+PRODUCTS_KITS_PATH = "/api/v1/products/kits/"
